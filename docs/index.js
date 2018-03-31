c5ac6f2bbae92b0d1df0e7f44340716c13e8bc36
hidao80 <hidao@smooththeway4u.net>
2018-03-31 13:56:21 +0900

    変更をdocsに反映

---
+++ docs/index.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+app.on('ready', () => {
+  // mainWindowを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
+  mainWindow = new BrowserWindow({width: 1000, height: 800});
+  // Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // ChromiumのDevツールを開く
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
