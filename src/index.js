daf41bf1b8581201a8b4f84038b2a048caf4ac93
hidao80 <hidao@smooththeway4u.net>
2018-03-31 13:55:26 +0900

    feat: ファイルシステムからMarkdownファイルを読み込み、表示します。

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow = null;
 app.on('ready', () => {
   // mainWindowを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
-  mainWindow = new BrowserWindow({width: 400, height: 300});
+  mainWindow = new BrowserWindow({width: 1000, height: 800});
   // Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
