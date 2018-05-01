4390296d230388f0aa5477b620d440358e84f5aa
hidao80 <USER_INFO@users.noreply.github.com>
2018-05-01 16:18:08 +0900

    refa: fix: タグを記事の1行目から取得する 同時にリファクタリングするダメなやーつ

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,20 +1,17 @@
 /*jshint esversion:6*/
-/*jslint devel: true */
 var timer;
 const WRITE_INTERVAL = 3000; 
 const hljs = require('highlight.js');
+const fs = require('fs');
+const config  = require('electron-config')();
 
 function writeMarkdownFile () {
-	const fs = require('fs');
-	const Config = require('electron-config');
-	let config = new Config();
-	
-	let currentFile = config.get('CURRENT_FILE');
-	let md = document.querySelector("#tsumiqiita-editor").value;
+	const currentFile = config.get('CURRENT_FILE');
+	const md = document.querySelector("#tsumiqiita-editor").value;
 
 	if (currentFile !== undefined ) {
 		if (currentFile.length > 0) {
-			let filePath = currentFile.replace(/\\/g, "\\\\");
+			const filePath = currentFile.replace(/\\/g, "\\\\");
 			fs.writeFileSync(filePath, md);
 			clearInterval(timer);
 			document.querySelector("#title").style.fontStyle = "normal";
@@ -34,24 +31,19 @@ function rerendaring() {
 			return ''; 
 		}
 	});
-	let md = document.querySelector("#tsumiqiita-editor").value;
+	const md = document.querySelector("#tsumiqiita-editor").value;
 	document.querySelector("#preview").innerHTML = it.render(md);
 }
 
 function rendering(path) {
-  const fs = require('fs');
-
 	fs.readFile(path, (error, text) => {
     if (error !== null) {
-//      alert('error : ' + error);
       return;
     }
     document.querySelector("#tsumiqiita-editor").value = text.toString();
+
 	rerendaring();
 
-	const Config = require('electron-config');
-	let config = new Config();
-		
 	config.set('CURRENT_FILE', path);
   });
 }
@@ -68,20 +60,17 @@ function autoSave() {
 }
 
 function setScrollSync() {
-	var p = document.querySelector('#preview');
-	var e = document.querySelector('#tsumiqiita-editor');
+	const p = document.querySelector('#preview');
+	const e = document.querySelector('#tsumiqiita-editor');
 	
 	p.onscroll = () => {e.scrollTop = p.scrollTop;};
 	e.onscroll = () => {p.scrollTop = e.scrollTop;};
 }
 
 function init() {
-	const Config = require('electron-config');
-  let config = new Config();
-	
-	let targetDir = config.get('TARGET_DIR');
+	const targetDir = config.get('TARGET_DIR');
 	if (targetDir === undefined) {targetDir = "";}
-  if (targetDir === "undefined") {targetDir = "";}
+	if (targetDir === "undefined") {targetDir = "";}
 	if (targetDir.length === 0) {
 		targetDir = "フォルダを選択してください";
 	} else {
@@ -89,10 +78,10 @@ function init() {
 	}
 	document.getElementById('target-dir').innerHTML = targetDir;
 
-	let currentFile = config.get('CURRENT_FILE');
+	const currentFile = config.get('CURRENT_FILE');
 	if (currentFile !== undefined ) {
 		if (currentFile.length > 0) {
-			let filePath = currentFile.replace(/\\/g, "\\\\");
+			const filePath = currentFile.replace(/\\/g, "\\\\");
 			rendering(filePath);
 		}
 	}
@@ -111,8 +100,6 @@ function selectTargetDir() {
 		title: 'フォルダの選択',
 		defaultPath: '.'
 	}, (folderNames) => {
-		const Config = require('electron-config');
-		let config = new Config();
 		config.set('TARGET_DIR', folderNames[0]);
 		document.getElementById('target-dir').innerHTML = folderNames[0];
 
@@ -122,7 +109,6 @@ function selectTargetDir() {
 
 // ファイルリストを取得。mdファイルのみ。
 function updateFileListPain(dir) {
-	const fs = require('fs');
 	const path = require('path');
 
 	if (dir.length === 0) {return;}
@@ -130,13 +116,13 @@ function updateFileListPain(dir) {
 		if (err) {throw err;}
 		let fileList = [];
 		files.filter( (file) => {
-			let target = dir + path.sep + file;
+			const target = dir + path.sep + file;
 			return fs.statSync(target).isFile() && /.*\.md$/.test(target); //絞り込み
 		}).forEach( (file) => {
 				fileList.push(file);
 		});
 
-    // 画面に反映
+	    // 画面に反映
 		let fileListHtml = "";
 		for(let fileName of fileList) {
 			let filePath = dir + path.sep + fileName;
@@ -151,16 +137,12 @@ function updateFileListPain(dir) {
 }
 
 function setToken() {
-	const Config = require('electron-config');
-  const config = new Config();
-
 	config.set("TOKEN", document.querySelector('#input').value);
 }
 
 function getTags() {
-	const text = document.querySelector("#tags").value;
+	const tags = document.querySelector("#tsumiqiita-editor").value.split("\n")[0].trim().split(" ");
 	let ret = [];
-	const tags = text.trim().split(" ");
 	
 	for(let i = 0; i < tags.length; i++) {
 		let tmp = tags[i].split(":");
@@ -175,23 +157,18 @@ function getTags() {
 }
 
 function post() {
-	const Config = require('electron-config');
-	const config = new Config();
-	
 	require('isomorphic-fetch');
 	const Qiita = require('qiita-js');
 	Qiita.setToken(config.get("TOKEN"));
 	Qiita.setEndpoint('https://qiita.com');
 		
 	const path = require('path');
-  const fs = require('fs');
 
-	let p = config.get("CURRENT_FILE");
-	let file = p.split(path.sep);
-	let filename = file[file.length-1];
-	let markdown = fs.readFileSync(p, 'utf-8');
-
-	let title = filename.replace(/(\.md)+$/,"");
+	const p = config.get("CURRENT_FILE");
+	const file = p.split(path.sep);
+	const filename = file[file.length-1];
+	const markdown = fs.readFileSync(p, 'utf-8').replace(/^.*\n/s, "");
+	const title = filename.replace(/(\.md)+$/,"");
 
 	var options = {
 		"body": markdown,
@@ -202,7 +179,7 @@ function post() {
 
 	//execution　api
 	Qiita.Resources.Item.create_item(options).then(function(res){
-		console.log(res);
+//		console.log(res);
 		if (res.message !== undefined) {
 			document.querySelector('#ng-dialog').showModal();
 		} else {
@@ -213,7 +190,6 @@ function post() {
 
 function createArticle() {
 	const Dialog = require('electron').remote.dialog;
-	const fs = require('fs');
 	
 	Dialog.showSaveDialog(null, {
 		title: '新規作成',
@@ -222,9 +198,6 @@ function createArticle() {
 				{name: 'Markdownファイル', extensions: ['md']},
 		]
 	}, (savedFiles) => {
-		const Config = require('electron-config');
-		let config = new Config();
-
 		try {
 			fs.writeFileSync(savedFiles, "");
 			config.set('CURRENT_FILE', savedFiles);
--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ app.on('ready', () => {
   // mainWindowを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
   mainWindow = new BrowserWindow({width: 1000, height: 800});
   // Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/../index.html');
 
   // ChromiumのDevツールを開く
   //mainWindow.webContents.openDevTools();
