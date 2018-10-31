200b63dfa6edc65cce3878d8f854121636d3053c
aoi <aoi@info-port.co.jp>
2018-10-31 12:42:27 +0900

    feat: テキストエリア内でtab入力できるように

--- src/js/functions.js
+++ src/js/functions.js
@@ -268,3 +268,24 @@ function createArticle() {
 		}
 	});
 }
+
+function OnTabKey( e, obj ){
+    // タブキーが押された時以外は即リターン
+    if( e.keyCode!=9 ){ return; }
+  
+    // タブキーを押したときのデフォルトの挙動を止める
+    e.preventDefault();
+  
+    // 現在のカーソルの位置と、カーソルの左右の文字列を取得しておく
+    var cursorPosition = obj.selectionStart;
+    var cursorLeft     = obj.value.substr( 0, cursorPosition );
+    var cursorRight    = obj.value.substr( cursorPosition, obj.value.length );
+  
+    // テキストエリアの中身を、
+    // 「取得しておいたカーソルの左側」+「タブ」+「取得しておいたカーソルの右側」
+    // という状態にする。
+    obj.value = cursorLeft+"\t"+cursorRight;
+  
+    // カーソルの位置を入力したタブの後ろにする
+    obj.selectionEnd = cursorPosition+1;
+}
